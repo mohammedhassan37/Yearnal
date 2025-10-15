f8662e1227244804aaafaf09c53dbd1388b21983
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-10-15 11:49:39 +0100

    Update server.js
    test

--- server/server.js
+++ server/server.js
@@ -9,23 +9,25 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// check 
-
 const pool = new pg.Pool({
-user:process.env.DB_USER,
-password:process.env.DB_PASSWORD,
-host:process.env.DB_HOST,
-port:process.env.DB_PORT,
-database:process.env.DB_NAME
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 app.get("/", async (req, res) => {
-  const result = await pool.query("SELECT NOW()");
-  res.json({ message: "Backend is working!", time: result.rows[0] });
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ message: "Backend is working!", time: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database error" });
+  }
 });
 
-
-const PORT = 5000 || process.env.DB_PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
