c582feec32b1118f9b7d12b8f9725e5f35532518
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-10-15 11:32:55 +0100

    Initialize React + Vite project with server setup
    Add initial project structure including React client with Vite, ESLint configuration, and basic server setup. Includes .gitignore files, README, configuration files, and starter source code for both client and server.

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import pg from 'pg';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+const pool = new pg.Pool({
+user:process.env.DB_USER,
+password:process.env.DB_PASSWORD,
+host:process.env.DB_HOST,
+port:process.env.DB_PORT,
+database:process.env.DB_NAME
+});
+
+app.get("/", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json({ message: "Backend is working!", time: result.rows[0] });
+});
+
+
+const PORT = 5000 || process.env.DB_PORT;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
