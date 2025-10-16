1634ee1294a84e5ab21c677716699ed5ad5e50a4
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-10-16 14:53:09 +0100

    html and css
    added back html and css

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,42 +1,51 @@
-import '../Styles/Login.css'
-
+import '../Styles/Login.css';
 
 function Login() {
-  
-
   return (
     <>
-       
-        <div className="LoginformContainer">
-        <div className="LoginformIntroduction">
-        <h1>Yearnal</h1>
-        <h3>Your journey begins here</h3>
+      <div className="LoginFormContainer">
+        <div className="LoginFormIntroduction">
+          <h1 id="FormIntroHeart">❤️</h1>
+          <h1 id="FormIntroTitle">Yearnal</h1>
+          <p id="FormIntroText">Your journey begins here</p>
         </div>
-        <div className="LoginformContent">
+
+        <div className="FormContainerMain">
+          <div className="FormContainerElements">
+            <div className="FormContainerBtns">
+              <button type="button" className="active">Login</button>
+              <button type="button">Sign up</button>
+            </div>
+
             <form>
-                <label for="full-name">Full Name </label> {/* Name*/}
-                <input type="text" id="full-name" className="info" required placeholder="John Doe"></input> 
-                <label for="email"> Email </label>
-                <input type="email" id="email" className="info" required placeholder="you@example.com"></input> {/* email */}
-                <label for="password">Password </label>
-                <input type="password" id="password" className="info" required minLength="8" placeholder="********"></input> {/* password */}
-                <label for="confirm-password">Confirm Password </label>
-                <input type="password" id="confirm-password" className="info" required minLength="8" placeholder="********"></input> {/* confirm password */}
-                <div className="terms"> {/* class for terms and to make the checkbox align better */}
-                <input type="checkbox" id="terms-of-service" required></input>
-                <label for="terms-of-service">I agree to the<a href='#'> Terms of Service</a> and <a href='#'> Privacy Policy</a> </label> {/* placeholder links */}
-                </div>
-                <button type="submit" className="createbutton">Create Account</button> {/* submit button, created class for accessibility in css */}
-                <div className="divider">
-                    <span>OR</span>
-                </div>
-                
-            
+              <div className="FormContainerLogin">
+                <label>Email</label>
+                <input
+                  type="email"
+                  placeholder="✉️ Enter Your Email"
+                />
 
+                <label>Password</label>
+                <input
+                  type="password"
+                  placeholder="🔐 Enter Your Password"
+                />
 
+                <label>Confirm Password</label>
+                <input
+                  type="password"
+                  placeholder="🔐 Confirm Password"
+                />
+
+                <button className="submitBtn" type="submit">
+                  Submit
+                </button>
+              </div>
             </form>
+          </div>
         </div>
-        </div>
+      </div>
+
     </>
   );
 }
