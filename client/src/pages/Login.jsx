e8f8d24e995e868c2d7295bcd7ef8efd558266fb
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-10-21 14:39:47 +0100

    Login,server
    added login frontend js to run with backend, changed backend to be related to postgre, not sql.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,30 @@
 import "../Styles/Login.css";
 import { Heart } from "lucide-react";
+import { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [message, setMessage] = useState();
+
+   const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:5000/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password, confirmPassword }),
+      });
+
+      const data = await res.json();
+      setMessage(data.message);
+    } catch (err) {
+      setMessage("Server error");
+    }
+  };
+
   return (
     <>
       <div className="LoginFormContainer">
@@ -22,26 +45,46 @@ function Login() {
               <button type="button">Sign up</button>
             </div>
 
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="FormContainerLogin">
                 <label>Email</label>
-                <input type="email" name="email" placeholder="✉️ Enter Your Email" required/>
+                <input  
+                  type="email" 
+                  name="email" 
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="✉️ Enter Your Email" 
+                  required/>
 
                 <label>Password</label>
-                <input type="password" name="password" placeholder="🔐 Enter Your Password" required/>
+                <input 
+                  type="password" 
+                  name="password" 
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="🔐 Enter Your Password" 
+                  required/>
 
                 <label>Confirm Password</label>
-                <input type="password" name="confirmPassword" placeholder="🔐 Confirm Password" required/>
+                <input 
+                  type="password" 
+                  name="confirmPassword" 
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  placeholder="🔐 Confirm Password" 
+                  required/>
 
                 <button className="submitBtn" type="submit">
                   Submit
                 </button>
+               
               </div>
             </form>
             <div className='LoginFormDivider'>
               <hr /> 
               <p>OR</p> 
               <hr />
+               <p>{message}</p>
             </div>
           </div>
         </div>
--- server/server.js
+++ server/server.js
@@ -31,9 +31,9 @@ const pool = new pg.Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false // required for Render
-  }
+  // ssl: {
+  //   rejectUnauthorized: false // required for Render
+  // }
 });
 
 
@@ -56,21 +56,25 @@ app.post("/signup", async (req,res) => {
     if(password !== confirmPassword){
       return res.status(400).json({success:false, message:"Passwords do not match"})
     }
-
-
-    const [existingUser] = await db.query(`SELECT * FROM users WHERE email = ?`, email)
-    if(existingUser.length > 0){
+    
+    // checks whether the user is pre-existent
+    const existingUser = await pool.query(`SELECT * FROM users WHERE email = $1`, [email])
+    if(existingUser.rows.length > 0){
       return res.status(400).json({success:false,message:"Email has already been registered"})
     }
-
+    
+    // encrypts the password
     const hashedPassword = await bcrypt.hash(password,10);
 
-   const [results] = await db.query
-   (`INSERT INTO users (email,hashedPassword) VALUES (?,?)`,
+    // creates the users
+    const results = await pool.query
+    (`INSERT INTO users (email,hashedPassword) VALUES ($1,$2)`,
     [ email, hashedPassword]);
 
+    // tells user the account is made
     return res.status(201).json({success:true,message:"User registered"})
-
+    
+    // catches any error or some shit
   }catch(err){
     console.error(err);
     return res.status(500).json({success:false,message:"Server error"})
